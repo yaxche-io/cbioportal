6cb38340148573aedafb85bb83bcf4f0ee59a710
Luke Sikina <lucas@ds.dfci.harvard.edu>
2020-02-28 10:55:55 -0500

    Treatments in study view

--- web/src/main/java/org/cbioportal/web/StudyViewController.java
+++ web/src/main/java/org/cbioportal/web/StudyViewController.java
@@ -686,7 +686,7 @@ public class StudyViewController {
                 .collect(Collectors.toList());
 
     }
-    
+
     private List<ClinicalData> fetchClinicalData(List<String> studyIds,
             List<String> sampleIds,
             List<String> patientIds,
