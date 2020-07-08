c7acddb782eb5474ceaa54ac7a2fe4a53e18356b
Luke Sikina <lucas@ds.dfci.harvard.edu>
2020-07-08 13:19:37 -0400

    Start test

--- service/src/test/java/org/cbioportal/service/impl/TreatmentServiceImplTest.java
+++ service/src/test/java/org/cbioportal/service/impl/TreatmentServiceImplTest.java
@@ -6,9 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TreatmentServiceImplTest {
